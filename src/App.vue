3a9faab88cce8a9196f84d3afdc16fcf99fd5b10
Leo <lion@lgcns.com>
2021-11-03 00:23:35 +0900

    db 스크립트 분리, vuex 추가, db crud 동작 로직

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,60 @@
   <img alt="Vue logo" src="./assets/logo.png" />
   <AppTitle msg="Welcome to Your Vue.js App" />
   <button @click="insertData">insert</button>
+  <button @click="getAllRecord">get all</button>
+  <button @click="getDetail">detail</button>
+  <div>
+    <ul>
+      <li v-for="item in alarmList" :key="item.alarmId">
+        {{ item.title }}
+        <button @click="updateData(item)">update</button>
+        <button @click="deleteData(item.alarmId)">delete</button>
+        <div v-if="state.showDetail">
+          <div>{{ item.title }}</div>
+          <div>{{ item.content }}</div>
+          <div>{{ item.alarmCycle }}</div>
+          <div>{{ item.alarmDate }}</div>
+          <div>{{ item.alarmTime }}</div>
+          <div>{{ item.isActivated }}</div>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 import AppTitle from "./components/AppTitle.vue";
+import { useStore } from "vuex";
+import { computed, reactive } from "@vue/reactivity";
 
 export default {
   name: "App",
   components: {
     AppTitle,
   },
-  method: {
-    insertData() {
+  setup() {
+    const store = useStore();
+    const alarmList = computed(() => store.state.alarm.alarmList);
+    const updateList = (alarmList) => store.commit("setAlarmList", alarmList);
+    const getAlarmDetail = (alarm) => store.commit("setSelectedAlarm", alarm);
+
+    const state = reactive({
+      showDetail: false,
+    });
+
+    const openSetting = () => {
+      chrome.tabs.create({
+        url: chrome.extension.getURL("index.html"),
+      });
+    };
+    const insertData = () => {
       chrome.runtime.sendMessage(
         {
           action: "INSERT",
           records: [
             {
-              title: "새알람",
-              content: "이따 이거 해야해",
+              title: "새알람2",
+              content: "이따 이거 해야해2",
               isActivated: true,
               alarmCycle: "ONCE",
               alarmDate: 1,
@@ -32,7 +67,70 @@ export default {
           console.log("this is callback", e);
         }
       );
-    },
+    };
+    const getAllRecord = () => {
+      chrome.runtime.sendMessage(
+        {
+          action: "SELECT_ALL",
+        },
+        function(res) {
+          console.log("111", res);
+          updateList(res);
+        }
+      );
+    };
+
+    const updateData = (item) => {
+      chrome.runtime.sendMessage(
+        {
+          action: "UPDATE",
+          records: { ...item, title: "업데이트 완료" },
+        },
+        (e) => {
+          console.log("this is callback", e);
+        }
+      );
+    };
+
+    const deleteData = (alarmId) => {
+      chrome.runtime.sendMessage(
+        {
+          action: "DELETE",
+          key: alarmId,
+        },
+        (e) => {
+          console.log("this is callback", e);
+        }
+      );
+    };
+
+    const getDetail = () => {
+      state.showDetail = !state.showDetail;
+      alarmList.forEach((v) => {
+        chrome.runtime.sendMessage(
+          {
+            action: "SELECT_BY_KEY",
+            key: v.alarmId,
+          },
+          function(res) {
+            console.log("222", res);
+            getAlarmDetail(res);
+          }
+        );
+      });
+    };
+
+    return {
+      alarmList,
+      updateList,
+      openSetting,
+      insertData,
+      getAllRecord,
+      updateData,
+      deleteData,
+      getDetail,
+      state,
+    };
   },
 };
 </script>
--- src/background.js
+++ src/background.js
@@ -1,156 +1,41 @@
-let db = null;
+import IndexedDB from "./scripts/IndexedDB";
+
 const DB_NAME = "alarmDB";
 const DB_VERSION = 1;
 const STORE_NAME_ALARM_CONTENT = "alarm_content";
-const KEY_PATH_ALARM_CONTENT = "alarm_id";
+const KEY_PATH_ALARM_CONTENT = "alarmId";
 
-function createDatabase(dbName, dbVersion, storeName, keyPath) {
-  const request = window.indexedDB.open(dbName, dbVersion);
-
-  request.onerror = function(event) {
-    console.log("Problem opening DB.");
-  };
-  request.onupgradeneeded = function(event) {
-    db = event.target.result;
-    let objectStore = db.createObjectStore(storeName, {
-      keyPath: keyPath,
-      autoIncrement: true,
-    });
-    objectStore.transaction.oncomplete = function(event) {
-      console.log("ObjectStore Created.");
-    };
-  };
-  request.onsuccess = function(event) {
-    db = event.target.result;
-    console.log("DB OPENED.");
-    db.onerror = function(event) {
-      console.log("FAILED TO OPEN DB.");
-    };
-  };
-}
+let idb = new IndexedDB(DB_NAME, DB_VERSION);
 
 chrome.runtime.onInstalled.addListener((event) => {
   console.log("application installed", event);
-  createDatabase(
-    DB_NAME,
-    DB_VERSION,
-    STORE_NAME_ALARM_CONTENT,
-    KEY_PATH_ALARM_CONTENT
-  );
+  idb.createDatabase(STORE_NAME_ALARM_CONTENT, KEY_PATH_ALARM_CONTENT);
 });
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log("Hello from the background", request, sender);
-  sendResponse(sender);
   switch (request.action) {
     case "INSERT":
-      insertRecords(request.records, STORE_NAME_ALARM_CONTENT);
+      idb.insertRecords(request.records, STORE_NAME_ALARM_CONTENT);
       break;
     case "SELECT_BY_KEY":
+      idb.getRecordByKey(request.key, STORE_NAME_ALARM_CONTENT).then((res) => {
+        sendResponse(res);
+      });
       break;
     case "SELECT_ALL":
+      idb.getAllRecord(STORE_NAME_ALARM_CONTENT).then((res) => {
+        sendResponse(res);
+      });
       break;
     case "DELETE":
+      idb.deleteRecordByKey(request.key, STORE_NAME_ALARM_CONTENT);
       break;
     case "UPDATE":
+      idb.updateRecord(request.records, STORE_NAME_ALARM_CONTENT);
       break;
 
     default:
       break;
   }
 });
-
-function insertRecords(records, storeName) {
-  if (db) {
-    const transaction = db.transaction(storeName, "readwrite");
-    const objectStore = transaction.objectStore(storeName);
-    return new Promise((resolve, reject) => {
-      transaction.oncomplete = function() {
-        console.log("ALL INSERT TRANSACTIONS COMPLETE.");
-        resolve(true);
-      };
-      transaction.onerror = function() {
-        console.log("PROBLEM INSERTING RECORDS.");
-        resolve(false);
-      };
-      records.forEach((data) => {
-        let request = objectStore.add(data);
-        request.onsuccess = function() {
-          console.log("Added: ", data);
-        };
-      });
-    });
-  }
-}
-
-function getRecordByKey(key, storeName) {
-  if (db) {
-    const transaction = db.transaction(storeName, "readonly");
-    const objectStore = transaction.objectStore(storeName);
-    return new Promise((resolve, reject) => {
-      transaction.oncomplete = function() {
-        console.log("ALL GET TRANSACTIONS COMPLETE.");
-      };
-      transaction.onerror = function() {
-        console.log("PROBLEM GETTING RECORDS.");
-      };
-      let request = objectStore.get(key);
-      request.onsuccess = function(event) {
-        resolve(event.target.result);
-      };
-    });
-  }
-}
-function getAllRecord(storeName) {
-  const transaction = db.transaction(storeName);
-  const objectStore = transaction.objectStore(storeName);
-
-  const request = objectStore.openCursor();
-
-  request.onsuccess = function() {
-    let cursor = request.result;
-    if (cursor) {
-      let key = cursor.key;
-      let value = cursor.value;
-      console.log(key, value);
-      cursor.continue();
-    } else {
-      console.log("No more data");
-    }
-  };
-}
-
-function updateRecord(record, storeName) {
-  if (db) {
-    const transaction = db.transaction(storeName, "readwrite");
-    const objectStore = transaction.objectStore(storeName);
-    return new Promise((resolve, reject) => {
-      transaction.oncomplete = function() {
-        console.log("ALL PUT TRANSACTIONS COMPLETE.");
-        resolve(true);
-      };
-      transaction.onerror = function() {
-        console.log("PROBLEM UPDATING RECORDS.");
-        resolve(false);
-      };
-      objectStore.put(record);
-    });
-  }
-}
-function deleteRecordByKey(key, storeName) {
-  if (db) {
-    const delete_transaction = db.transaction(storeName, "readwrite");
-    const objectStore = delete_transaction.objectStore(storeName);
-    return new Promise((resolve, reject) => {
-      delete_transaction.oncomplete = function() {
-        console.log("ALL DELETE TRANSACTIONS COMPLETE.");
-        resolve(true);
-      };
-      delete_transaction.onerror = function() {
-        console.log("PROBLEM DELETE RECORDS.");
-        resolve(false);
-      };
-      objectStore.delete(key);
-    });
-  }
-}
